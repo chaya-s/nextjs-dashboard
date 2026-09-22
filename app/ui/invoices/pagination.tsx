cfdb2f250b7589409a3016f8632796c44b882a12
chaya-s <chayasuresh2854@gmail.com>
2026-09-22 18:05:16 +0530

    Add streaming search pagination and invoice actions

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,50 +4,70 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
-export default function Pagination({ totalPages }: { totalPages: number }) {
-  // NOTE: Uncomment this code in Chapter 10
+export default function Pagination({
+  totalPages,
+}: {
+  totalPages: number;
+}) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+
+    params.set('page', pageNumber.toString());
+
+    return `${pathname}?${params.toString()}`;
+  };
+
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
-      {/*  NOTE: Uncomment this code in Chapter 10 */}
+      <PaginationArrow
+        direction="left"
+        href={createPageURL(currentPage - 1)}
+        isDisabled={currentPage <= 1}
+      />
 
-      {/* <div className="inline-flex">
-        <PaginationArrow
-          direction="left"
-          href={createPageURL(currentPage - 1)}
-          isDisabled={currentPage <= 1}
-        />
+      <div className="flex -space-x-px">
+        {allPages.map((page, index) => {
+          let position:
+            | 'first'
+            | 'last'
+            | 'single'
+            | 'middle'
+            | undefined;
 
-        <div className="flex -space-x-px">
-          {allPages.map((page, index) => {
-            let position: 'first' | 'last' | 'single' | 'middle' | undefined;
+          if (index === 0) position = 'first';
 
-            if (index === 0) position = 'first';
-            if (index === allPages.length - 1) position = 'last';
-            if (allPages.length === 1) position = 'single';
-            if (page === '...') position = 'middle';
+          if (index === allPages.length - 1) position = 'last';
 
-            return (
-              <PaginationNumber
-                key={`${page}-${index}`}
-                href={createPageURL(page)}
-                page={page}
-                position={position}
-                isActive={currentPage === page}
-              />
-            );
-          })}
-        </div>
+          if (allPages.length === 1) position = 'single';
 
-        <PaginationArrow
-          direction="right"
-          href={createPageURL(currentPage + 1)}
-          isDisabled={currentPage >= totalPages}
-        />
-      </div> */}
+          if (page === '...') position = 'middle';
+
+          return (
+            <PaginationNumber
+              key={`${page}-${index}`}
+              href={createPageURL(page)}
+              page={page}
+              position={position}
+              isActive={currentPage === page}
+            />
+          );
+        })}
+      </div>
+
+      <PaginationArrow
+        direction="right"
+        href={createPageURL(currentPage + 1)}
+        isDisabled={currentPage >= totalPages}
+      />
     </>
   );
 }
@@ -67,9 +87,13 @@ function PaginationNumber({
     'flex h-10 w-10 items-center justify-center text-sm border',
     {
       'rounded-l-md': position === 'first' || position === 'single',
+
       'rounded-r-md': position === 'last' || position === 'single',
+
       'z-10 bg-blue-600 border-blue-600 text-white': isActive,
+
       'hover:bg-gray-100': !isActive && position !== 'middle',
+
       'text-gray-300': position === 'middle',
     },
   );
@@ -77,7 +101,7 @@ function PaginationNumber({
   return isActive || position === 'middle' ? (
     <div className={className}>{page}</div>
   ) : (
-    <Link href={href} className={className}>
+    <Link className={className} href={href}>
       {page}
     </Link>
   );
@@ -96,8 +120,11 @@ function PaginationArrow({
     'flex h-10 w-10 items-center justify-center rounded-md border',
     {
       'pointer-events-none text-gray-300': isDisabled,
+
       'hover:bg-gray-100': !isDisabled,
+
       'mr-2 md:mr-4': direction === 'left',
+
       'ml-2 md:ml-4': direction === 'right',
     },
   );
